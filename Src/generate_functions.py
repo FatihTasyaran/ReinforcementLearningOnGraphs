835d0492bff13c9e23737de398aa305dfe4b6807
fatihtasyaran <fatihtasyaran@sabanciuniv.edu>
2020-01-07 06:24:53 +0300

    Reform

---
+++ Src/generate_functions.py
@@ -0,0 +1,9 @@
+import graph_generator
+
+no_states = 45
+density = 0.4
+cardinality = 22
+algorithm = 'strong' #'weak or strong or strong2'
+no = '1' #'number of same graph'
+
+graph_generator.main(no_states, density, cardinality, algorithm, no)
