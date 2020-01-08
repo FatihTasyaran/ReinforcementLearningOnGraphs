972d25e5a3784bfeafed4738b27498f7891f77c7
fatihtasyaran <fatihtasyaran@sabanciuniv.edu>
2020-01-08 20:41:21 +0300

    Almost there

--- Src/distance.py
+++ Src/distance.py
@@ -1,162 +1,25 @@
 import graph_reader
 import networkx as nx
 
+pathS = '/home/fatih/Documents/CS560/project/Data/10s/55_0.3_8_strong2_#4.csv'
+pathB = '/home/fatih/Documents/CS560/project/Data/generating4/2000_0.2_17_strong2_#1.csv'
+
+graphS = graph_reader.return_graph(pathS)
+nx_graphS = graph_reader.convert_to_networkX(graphS)
 
 
+graphB = graph_reader.return_graph(pathB)
+nx_graphB = graph_reader.convert_to_networkX(graphB)
 
-file1 = '20_0.2_10_strong2_#1.csv'
-file2 = '20_0.7_10_strong2_#1.csv'
-len1 = 20
+print('Small')
+print(nx.shortest_path_length(nx_graphS,source=0,target=1))
+print(nx.shortest_path_length(nx_graphS,source=1,target=2))
+print(nx.shortest_path_length(nx_graphS,source=2,target=32))
 
-file3 = '200_0.2_15_strong2_#1.csv'
-file4 = '200_0.7_15_strong2_#1.csv'
-len2 = 200
+print('Big')
+print(nx.shortest_path_length(nx_graphB,source=0,target=1))
+print(nx.shortest_path_length(nx_graphB,source=1,target=2))
+print(nx.shortest_path_length(nx_graphB,source=2,target=1674))
 
-file5 = '2000_0.2_17_strong2_#1.csv'
-file6 = '2000_0.7_17_strong2_#1.csv'
-len3 = 2000
-
-file7 = '100000_0.2_20_strong2_#1.csv'
-file8 = '100000_0.7_20_strong2_#1.csv'
-len4 = 2000
-
-
-
-path = 'generating4/'
-graph = graph_reader.return_graph(path+file1)
-nx_graph = graph_reader.convert_to_networkX(graph)
-
-my_max = 0
-twos = -1
-max_dist = -1
-for i in range(len1):
-    dist = nx.shortest_path_length(nx_graph,source=i,target=7)
-    if (dist > my_max):
-        my_max = i
-        max_dist = dist
-    if (dist == 2):
-        twos = i
-
-print(file1, ':  max: ', my_max, ' two: ', twos, ' max_dist: ', max_dist)
-
-
-graph = graph_reader.return_graph(path+file2)
-nx_graph = graph_reader.convert_to_networkX(graph)
-
-my_max = 0
-twos = -1
-max_dist = -1
-for i in range(len1):
-    dist = nx.shortest_path_length(nx_graph,source=i,target=3)
-    if (dist > my_max):
-        my_max = i
-        max_dist = dist
-    if (dist == 2):
-        twos = i
-
-print(file2, ':  max: ', my_max, ' two: ', twos, ' max_dist: ', max_dist)
-    
-
-
-graph = graph_reader.return_graph(path+file3)
-nx_graph = graph_reader.convert_to_networkX(graph)
-
-my_max = 0
-twos = -1
-max_dist = -1
-for i in range(len1):
-    dist = nx.shortest_path_length(nx_graph,source=i,target=199)
-    if (dist > my_max):
-        my_max = i
-        max_dist = dist
-    if (dist == 2):
-        twos = i
-
-print(file3, ':  max: ', my_max, ' two: ', twos, ' max_dist: ', max_dist)
-
-
-graph = graph_reader.return_graph(path+file4)
-nx_graph = graph_reader.convert_to_networkX(graph)
-
-my_max = 0
-twos = -1
-max_dist = -1
-for i in range(len1):
-    dist = nx.shortest_path_length(nx_graph,source=i,target=49)
-    if (dist > my_max):
-        my_max = i
-        max_dist = dist
-    if (dist == 2):
-        twos = i
-        
-print(file4, ':  max: ', my_max, ' two: ', twos, ' max_dist: ', max_dist)
-
-
-graph = graph_reader.return_graph(path+file5)
-nx_graph = graph_reader.convert_to_networkX(graph)
-
-my_max = 0
-twos = -1
-max_dist = -1
-for i in range(len1):
-    dist = nx.shortest_path_length(nx_graph,source=i,target=1674)
-    if (dist > my_max):
-        my_max = i
-        max_dist = dist
-    if (dist == 2):
-        twos = i
-
-print(file5, ':  max: ', my_max, ' two: ', twos, ' max_dist: ', max_dist)
-
-
-graph = graph_reader.return_graph(path+file6)
-nx_graph = graph_reader.convert_to_networkX(graph)
-
-my_max = 0
-twos = -1
-max_dist = -1
-for i in range(len1):
-    dist = nx.shortest_path_length(nx_graph,source=i,target=822)
-    if (dist > my_max):
-        my_max = i
-        max_dist = dist
-    if (dist == 2):
-        twos = i
-
-print(file6, ':  max: ', my_max, ' two: ', twos, ' max_dist: ', max_dist)
-
-
-graph = graph_reader.return_graph(path+file7)
-nx_graph = graph_reader.convert_to_networkX(graph)
-
-my_max = 0
-twos = -1
-max_dist = -1
-for i in range(len1):
-    dist = nx.shortest_path_length(nx_graph,source=i,target=32682)
-    if (dist > my_max):
-        my_max = i
-        max_dist = dist
-    if (dist == 2):
-        twos = i
-
-print(file7, ':  max: ', my_max, ' two: ', twos, ' max_dist: ', max_dist)
-
-
-graph = graph_reader.return_graph(path+file8)
-nx_graph = graph_reader.convert_to_networkX(graph)
-
-my_max = 0
-twos = -1
-max_dist = -1
-for i in range(len1):
-    dist = nx.shortest_path_length(nx_graph,source=i,target=92124)
-    if (dist > my_max):
-        my_max = i
-        max_dist = dist
-    if (dist == 2):
-        twos = i
-
-print(file8, ':  max: ', my_max, ' two: ', twos, ' max_dist: ', max_dist)
 
 
--- Src/two_interaction.py
+++ Src/two_interaction.py
@@ -5,23 +5,17 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 
-max_episode = 100
+max_episode = 2500
 learning_rate = 0.9
-discount = 0.75
+discount = 0.9
 epsilon = 1##Start with exploration
 decay_rate = 1/(max_episode-(max_episode/5)) ##First half of episodes favor exploration,
                                ##Second half favor exploitation
 max_q_depth = 1
-CHECKPOINT1_FOUND = False
-CHECKPOINT2_FOUND = False
-CHECKPOINT1 = 1
-CHECKPOINT2 = 2
+CHECKPOINT_FOUND = False
+CHECKPOINT = 19
 
-#CHECKPOINT1 = 22
-#CHECKPOINT2 = 5
-
-
-def calculate_new_q(rewards1, rewards2, rewards3 ,q_table, action, old_state, new_state):
+def calculate_new_q(rewards1, rewards2, q_table, action, old_state, new_state):
     ##IMPLEMENTING BELLMAN OPTIMALITY
     old = (1-learning_rate)*q_table[old_state][action]
 
@@ -34,13 +28,8 @@ def calculate_new_q(rewards1, rewards2, rewards3 ,q_table, action, old_state, ne
 
     rewards = rewards1
 
-    if(CHECKPOINT1_FOUND and not CHECKPOINT2_FOUND):
+    if(CHECKPOINT_FOUND):
         rewards = rewards2
-
-    if(CHECKPOINT1_FOUND and CHECKPOINT2_FOUND):
-        rewards = rewards3
-
-    
     
     #print('len rewards:', rewards)
     new = learning_rate * (rewards[new_state] + discount*max_q)
@@ -147,21 +136,18 @@ def sarsa_calculate(graph, rewards, q_table, action, old_state, new_state):
 
     q_table[old_state][action] = old + new
 
-def q_learn(rewards1, rewards2, rewards3, framework_graph, graph, q_table, start):
+def q_learn_2_interaction(rewards1, rewards2, framework_graph, graph, q_table, start):
     #print('decay rate:', decay_rate)
     #print('epsilon goes to 0 in:', 1/decay_rate, ' episodes')
 
     global epsilon
     global decay_rate
-    global CHECKPOINT1_FOUND
-    global CHECKPOINT2_FOUND
+    global CHECKPOINT_FOUND
 
     total_rewards = []
     found_in = []
-    checkp1 = []
-    checkp2 = []
+    checkp = []
     eps = []
-    learned_min = []
     
     for episode in range(max_episode):
         print('****EPISODE ', episode, ' ****')
@@ -178,19 +164,19 @@ def q_learn(rewards1, rewards2, rewards3, framework_graph, graph, q_table, start
                 if(graph[state][0][le][1] == action):
                     new = graph[state][0][le][0]
 
-            #print(state, '->', new)
-            calculate_new_q(rewards1, rewards2, rewards3, q_table, action, state, new)
+            print(state, '->', new)
+            calculate_new_q(rewards1, rewards2, q_table, action, state, new)
             state = new
 
-            if(state == CHECKPOINT1):
-                CHECKPOINT1_FOUND = True
+            if(state == CHECKPOINT):
+                CHECKPOINT_FOUND = True
                 found = True
                 print('Checkpoint Found in: ', time_step, ' steps')
-                checkp1.append(time_step)
+                checkp.append(time_step)
 
             time_step += 1
 
-            if(CHECKPOINT1_FOUND):
+            if(CHECKPOINT_FOUND):
                 end = False
                 while(not end):
                     action = choose_action(graph, q_table, state)
@@ -199,44 +185,19 @@ def q_learn(rewards1, rewards2, rewards3, framework_graph, graph, q_table, start
                         if(graph[state][0][le][1] == action):
                             new = graph[state][0][le][0]
 
-                    #print(state, '->', new)
-                    calculate_new_q(rewards1, rewards2, rewards3, q_table, action, state, new)
+                    print(state, '->', new)
+                    calculate_new_q(rewards1, rewards2, q_table, action, state, new)
                     state = new
-
-                    if(state == CHECKPOINT2):
-                        CHECKPOINT2_FOUND = True
-                        end = True
-                        print('Checkpoint Found in: ', time_step, ' steps')
-                        checkp2.append(time_step)
-
-                    time_step += 1
-
-                    if(CHECKPOINT2_FOUND):
-                        last = False
-
-                        while(not last):
-
-                            action = choose_action(graph, q_table, state)
-                    
-                            for le in range(len(graph[state][0])):
-                                if(graph[state][0][le][1] == action):
-                                    new = graph[state][0][le][0]
-
-                            #print(state, '->', new)
-                            calculate_new_q(rewards1, rewards2, rewards3, q_table, action, state, new)
-                            state = new
             
-                            if(framework_graph[state]['Faulty'] == True):
-                                last = True
-                                print('Found in:', time_step, ' time steps')
-
-                            time_step += 1
+                    if(framework_graph[state]['Faulty'] == True):
+                        end = True
+                        print('Found in:', time_step, ' time steps')
+                    time_step += 1
                         
                     
                 
         epsilon -= decay_rate
-        CHECKPOINT1_FOUND = False
-        CHECKPOINT2_FOUND = False
+        CHECKPOINT_FOUND = False
         #print(q_table)
 
         ####CALCULATING TOTAL REWARD AT THE END OF THE EPISODE####
@@ -256,57 +217,27 @@ def q_learn(rewards1, rewards2, rewards3, framework_graph, graph, q_table, start
     x = []
     for i in range(max_episode):
         x.append(i)
-
-    learned_min.append(checkp1[max_episode-1])
-    learned_min.append(checkp2[max_episode-1])
-    learned_min.append(found_in[max_episode-1])
-
-    eps = 'null'
-    if(decay_rate == 0):
-        eps = str(epsilon) + ' Epsilon Greedy'
-    else:
-        eps = 'Epsilon Decaying'
-    
-    tit = str(len(framework_graph))
-    title = tit + ' nodes ' + eps
         
     plt.subplot(3,1,1)
-    plt.title(title)
     plt.plot(x, total_rewards, label = 'Total Reward', color='black')
     #plt.plot(x, found_in, label = 'Time Steps', color='red')
     plt.ylabel('Total Reward')
 
     plt.subplot(3,1,2)
-    plt.plot(x, checkp1, label = 'Check Point1', color='green')
-    plt.plot(x, checkp2, label = 'Check Point2', color='purple')
+    plt.plot(x, checkp, label = 'Check Point', color='green')
     plt.plot(x, found_in, label = 'Fault', color='red')
     plt.ylabel('Found in Time Steps')
     plt.legend()
 
-    tot_c1 = sum(checkp1)
-    tot_c2 = sum(checkp2)
-    tot_f = sum(found_in)
-
-    totals = [tot_c1, tot_c2, tot_f]
-    xx = ['CHECKPOINT1', 'CHECKPOINT2', 'FAULTY']
-    collors = ['green', 'purple', 'red']
-
-    plt.subplot(3,1,3)
-    plt.title('Total Time Steps Spent')
-    plt.bar(xx, totals, color=collors)
-
-    '''
     plt.subplot(3,1,3)
     plt.plot(x, eps, label = 'Epsilon', color='cyan')
     plt.ylabel('Epsilon Change')
-    '''
 
     plt.tight_layout()
-    #plt.show()
-    name = str(epsilon) + '_' + str(max_episode) + '.pdf'
-    plt.savefig(name)
-    
-    return total_rewards, found_in, eps, learned_min
+    plt.show()
+    #plt.savefig('checkpoint.pdf')
+
+    return total_rewards, found_in, eps
     ####DRAWING # OF TIME STEPS VS EPISODES AND REWARD####
 
 def sarsa(rewards, framework_graph, graph, q_table, start):
@@ -593,28 +524,23 @@ def get_rewards(framework_graph):
     global CHECKPOINT
     rewards1 = []
     rewards2 = []
-    rewards3 = []
 
     for i in range(len(framework_graph)):
-        if(i == CHECKPOINT1):
-            rewards1.append(10)
+        if(i == CHECKPOINT):
+            rewards1.append(5)
         else:
-            rewards1.append(-2)
-
-    for i in range(len(framework_graph)):
-        if(i == CHECKPOINT2):
-            rewards2.append(10)
-        else:
-            rewards2.append(-2)
+            rewards1.append(-1)
 
     for i in range(len(framework_graph)):
         if(framework_graph[i]['Faulty'] == True):
-            rewards3.append(10)
+            rewards2.append(5)
+        if(i == CHECKPOINT):
+            rewards1.append(0)
         else:
-            rewards3.append(-2)
+            rewards2.append(-1)
     ##SETTING UP THE REWARDS##
 
-    return rewards1, rewards2, rewards3
+    return rewards1, rewards2
 
 def main():
     
@@ -635,148 +561,19 @@ def main():
     
     #####ONE EXPERIMENT RUNTIME Q_LEARN#####
     epsilon = 1
-    decay_rate = 1/(max_episode-(max_episode/5)) 
     q_table = empty_q_table(graph, alphabet_card)
-    rewards1, rewards2, rewards3 = get_rewards(framework_graph)
-    q_tot, q_found, eps, one_learned_min = q_learn(rewards1, rewards2, rewards3, framework_graph, graph, q_table, 0)
-    #####ONE EXPERIMENT RUNTIME Q_LEARN####
-
-    #####ONE EXPERIMENT RUNTIME Q_LEARN#####
-    epsilon = 0.1
-    decay_rate = 0
-    q_table = empty_q_table(graph, alphabet_card)
-    rewards1, rewards2, rewards3 = get_rewards(framework_graph)
-    q_tot, q_found, eps, onef_learned_min = q_learn(rewards1, rewards2, rewards3, framework_graph, graph, q_table, 0)
-    #####ONE EXPERIMENT RUNTIME Q_LEARN####
-
-    #####ONE EXPERIMENT RUNTIME Q_LEARN#####
-    epsilon = 0.01
-    decay_rate = 0
-    q_table = empty_q_table(graph, alphabet_card)
-    rewards1, rewards2, rewards3 = get_rewards(framework_graph)
-    q_tot, q_found, eps, twof_learned_min= q_learn(rewards1, rewards2, rewards3, framework_graph, graph, q_table, 0)
+    rewards1, rewards2 = get_rewards(framework_graph)
+    q_tot, q_found, eps = q_learn_2_interaction(rewards1, rewards2, framework_graph, graph, q_table, 0)
     #####ONE EXPERIMENT RUNTIME Q_LEARN####
 
     #####ONE EXPERIMENT RUNTIME Q_LEARN#####
     epsilon = 0.001
     decay_rate = 0
     q_table = empty_q_table(graph, alphabet_card)
-    rewards1, rewards2, rewards3 = get_rewards(framework_graph)
-    q_tot, q_found, eps , threef_learned_min= q_learn(rewards1, rewards2, rewards3, framework_graph, graph, q_table, 0)
+    rewards1, rewards2 = get_rewards(framework_graph)
+    q_tot, q_found, eps = q_learn_2_interaction(rewards1, rewards2, framework_graph, graph, q_table, 0)
     #####ONE EXPERIMENT RUNTIME Q_LEARN####
 
-    collors = ['black', 'purple', 'cyan', 'red', 'green']
-
-    real_small = [2,5,7]
-
-    '''
-
-    plt.subplot(3,1,1)
-    titr = str(max_episode) + ' Episodes 55 Nodes Learned Min Distances \n Start to CHECKPOINT 1'
-    plt.title(titr)
-    xx = ['Real','Decaying', '0.1', '0.01', '0.001']
-    vals = [real_small[0], one_learned_min[0], onef_learned_min[0], twof_learned_min[0], threef_learned_min[0]]
-    plt.bar(xx, vals, color=collors)
-    
-
-
-    plt.subplot(3,1,2)
-    plt.title('CHECKPOINT 1 to CHECKPOINT 2')
-    xx = ['Real','Decaying', '0.1', '0.01', '0.001']
-    vals = [real_small[1], one_learned_min[1], onef_learned_min[1], twof_learned_min[1], threef_learned_min[1]]
-    plt.bar(xx, vals, color=collors)
-    
-
-
-    plt.subplot(3,1,3)
-    plt.title('CHECKPOINT 2 to FAULTY')
-    xx = ['Real','Decaying', '0.1', '0.01', '0.001']
-    vals = [real_small[1], one_learned_min[1], onef_learned_min[1], twof_learned_min[1], threef_learned_min[1]]
-    plt.bar(xx, vals, color=collors)
-
-    plt.tight_layout()
-    #plt.show()
-
-    plt.savefig('100_small_learned_min.pdf')
-    
-    '''
-    
-
-
-    ####BIGGER#####
-    framework_graph = graph_reader.return_graph('/home/fatih/Documents/CS560/proje\
-ct/Data/generating4/2000_0.2_17_strong2_#1.csv')
-    alphabet_card = 25
-    graph = convert_to_list_representation(framework_graph)
-    ####BIGGER#####
-
-
-    #####ONE EXPERIMENT RUNTIME Q_LEARN#####
-    epsilon = 1
-    decay_rate = 1/(max_episode-(max_episode/5)) 
-    q_table = empty_q_table(graph, alphabet_card)
-    rewards1, rewards2, rewards3 = get_rewards(framework_graph)
-    q_tot, q_found, eps, one_learned_min= q_learn(rewards1, rewards2, rewards3, framework_graph, graph, q_table, 0)
-    #####ONE EXPERIMENT RUNTIME Q_LEARN####
-
-    #####ONE EXPERIMENT RUNTIME Q_LEARN#####
-    epsilon = 0.1
-    decay_rate = 0
-    q_table = empty_q_table(graph, alphabet_card)
-    rewards1, rewards2, rewards3 = get_rewards(framework_graph)
-    q_tot, q_found, eps, onef_learned_min = q_learn(rewards1, rewards2, rewards3, framework_graph, graph, q_table, 0)
-    #####ONE EXPERIMENT RUNTIME Q_LEARN####
-
-    #####ONE EXPERIMENT RUNTIME Q_LEARN#####
-    epsilon = 0.01
-    decay_rate = 0
-    q_table = empty_q_table(graph, alphabet_card)
-    rewards1, rewards2, rewards3 = get_rewards(framework_graph)
-    q_tot, q_found, eps, twof_learned_min = q_learn(rewards1, rewards2, rewards3, framework_graph, graph, q_table, 0)
-    #####ONE EXPERIMENT RUNTIME Q_LEARN####
-
-    #####ONE EXPERIMENT RUNTIME Q_LEARN#####
-    epsilon = 0.001
-    decay_rate = 0
-    q_table = empty_q_table(graph, alphabet_card)
-    rewards1, rewards2, rewards3 = get_rewards(framework_graph)
-    q_tot, q_found, eps, threef_learned_min = q_learn(rewards1, rewards2, rewards3, framework_graph, graph, q_table, 0)
-    #####ONE EXPERIMENT RUNTIME Q_LEARN####
-
-    collors = ['black', 'purple', 'cyan', 'red', 'green']
-
-    real_small = [3,5,8]
-
-    '''
-
-    plt.subplot(3,1,1)
-    titr = str(max_episode) + ' Episodes 2000 Nodes Learned Min Distances \n Start to CHECKPOINT 1'
-    plt.title(titr)
-    xx = ['Real','Decaying', '0.1', '0.01', '0.001']
-    vals = [real_small[0], one_learned_min[0], onef_learned_min[0], twof_learned_min[0], threef_learned_min[0]]
-    plt.bar(xx, vals, color=collors)
-    
-
-
-    plt.subplot(3,1,2)
-    plt.title('CHECKPOINT 1 to CHECKPOINT 2')
-    xx = ['Real','Decaying', '0.1', '0.01', '0.001']
-    vals = [real_small[1], one_learned_min[1], onef_learned_min[1], twof_learned_min[1], threef_learned_min[1]]
-    plt.bar(xx, vals, color=collors)
-    
-
-
-    plt.subplot(3,1,3)
-    plt.title('CHECKPOINT 2 to FAULTY')
-    xx = ['Real','Decaying', '0.1', '0.01', '0.001']
-    vals = [real_small[1], one_learned_min[1], onef_learned_min[1], twof_learned_min[1], threef_learned_min[1]]
-    plt.bar(xx, vals, color=collors)
-
-    plt.tight_layout()
-    #plt.show()
-    plt.savefig('100_big_learned_min.pdf')
-
-    '''
 
 if __name__ == '__main__':
     random.seed(os.urandom(10000))
